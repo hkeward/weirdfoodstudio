1e364233ecb7f2180dc2b838d3e9bc68341fa805
Heather Ward <heather@dnastack.com>
2021-05-28 09:15:17 -0400

    Initial basic webpage

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <title-bar />
+  <div id="navbar_contents">
+    <div id="nav">
+      <div>
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>
+    <div>
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import TitleBar from "@/components/TitleBar.vue";
+
+export default {
+  components: {
+    TitleBar,
+  },
+};
+</script>
+
+<style lang="less">
+#app {
+  font-family: "Titillium Web", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  text-align: left;
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+#navbar_contents {
+  display: flex;
+}
+</style>
---
+++ src/components/TitleBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="title_bar">
+    <div id="text_display">
+      <router-link to="/">
+        <div id="title_text">Weird Food Studio</div>
+      </router-link>
+      <div>
+        <div id="artist">Jessi Wood</div>
+      </div>
+    </div>
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+#title_bar {
+  font-family: "Squada One", cursive;
+  display: flex;
+}
+#text_display a {
+  text-decoration: none;
+}
+#title_text {
+  font-size: 6em;
+  text-align: left;
+  color: #2c3e50;
+  text-decoration: none;
+}
+#artist {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
